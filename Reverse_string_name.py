1cf0466cdf31c3bb69bed06b6bd56e65f3003a96
chiku1697 <85185040+chiku1697@users.noreply.github.com>
2021-06-06 00:03:08 +0530

    Add files via upload
    Tasks done

---
+++ Reverse_string_name.py
@@ -0,0 +1,4 @@
+# for reverse indexing at any string use -1, right to left
+s = 'Pravesh'[::-1]
+print(s)
+
---
+++ count_character.py
@@ -0,0 +1,24 @@
+##Write a .py script to get the occurances of a character (irrespective case sensitivity) from a sentence and get the index of that character as well.
+##For example the sentence is = "Python is a preferred programming language in today world of informations" and we need to get it for "p". Input should be provided by a variable.
+
+##There are two ways that i defined here, one to take the input from the user and second is of fixed character
+
+#1
+
+count = 0
+string = 'Python is a preferred programming language in today world of informations'
+in_value =input('which character you want to count: ')
+for i in string.lower():
+    if i == in_value.lower():
+        count = count + 1
+print(count)
+
+
+#2
+
+##count = 0
+##string = 'Python is a preferred programming language in today world of informations'
+##for i in string.lower():
+##    if i == 'p':
+##        count = count + 1
+##print(count)
