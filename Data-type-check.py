47fe2a4217ca88d7b43e22aae81d88d8b674b316
chiku1697 <85185040+chiku1697@users.noreply.github.com>
2021-06-17 22:09:20 +0530

    check for correct data type used

---
+++ Data-type-check.py
@@ -0,0 +1,15 @@
+name = input('Your Name: ')
+num = '0123456789'
+for i in name:
+    if i in num:
+        print('wrong character in name as: ' + i)
+    else:
+        ()
+print()
+
+phone = input('your phone number: ')
+for j in phone:
+    if j not in num:
+        print('wrong character in phone as: ' + j)
+    else:
+        ()
