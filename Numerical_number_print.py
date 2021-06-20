5174191708b165de8a5ce0518825c5ad88f0a19b
chiku1697 <85185040+chiku1697@users.noreply.github.com>
2021-06-20 23:35:36 +0530

    1-10 or odd values print kind of task

---
+++ Numerical_number_print.py
@@ -0,0 +1,23 @@
+##========Print 1 to 20 only odd values=======
+
+##for i in range(20):
+##    if (i % 2) > 0:
+##        print(i)
+
+##i = 1
+##while i < 20:
+##    if (i % 2) > 0:
+##        print(i)
+##    i+=1
+
+##for i in range(1,20,2):
+##    print(i)
+
+##=========Print 1 to 10 in descending order=====
+
+##for i in range(10):
+##    print(10-i)
+
+
+##for i in range(10,0,-1):
+##    print(i)
